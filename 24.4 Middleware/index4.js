bfe3a6db2a7ea887c2fc6163b4a0a7a324dba869
justindnguyen <justindnguyen03@gmail.com>
2025-03-31 19:40:09 -0700

    update

--- 24.4 Middleware/index4.js
+++ 24.4 Middleware/index4.js
@@ -1,7 +1,33 @@
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+
+app.get("/", (req, res) => {
+  console.log(__dirname + "/public/index.html");
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+var bandName = "";
+
+function bandNameGenerator(req, res, next) {
+  console.log(req.body);
+  bandName = req.body.street + req.body.pet;
+  next();
+}
+
+app.use(bandNameGenerator);
+
+app.post("/submit", (req, res) => {
+  res.send(`<h1>Your band name is:</h1><h2>${bandName}</h2>`);
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
